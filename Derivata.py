ed5360a38fc58cb3d9baea69dc38dd0754f5b15f
MaxB3435 <bortasmax@gmail.com>
2024-03-06 15:02:49 +0100

    Update Derivata.py

--- Derivata.py
+++ Derivata.py
@@ -3,13 +3,21 @@ import numpy as np
 
 func = input("Ange funktion")
 
-def derivate(f,a,h):
-    f = lambda x:eval(func)
-    return((f(a+h)-f(a))/h)
+#def derivate(f,a,h):
+#    f = lambda x:eval(func)
+#    return((f(a+h)-f(a))/h)
     
-print(derivate(func,2,0.00001))
+#print(derivate(func,2,0.00001))
 
 def integrate(f,a,b,dx):
+    int = 0
+    f = lambda x:eval(func)
+    while a < b:
+        int = int + (f(a)*dx)
+        a = a + dx
+    return int
+print(integrate(func,0,2,0.0001))
+
     
 #plt.plot(x,y)
 #plt.grid()
